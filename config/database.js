7ddd3697588b0eaa95ef56f2e7e550fd672ae642
ashishkr16 <kumar.ashish.raj1997@gmail.com>
2021-09-28 11:08:47 +0530

    updated database details

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-module.exports = new Sequelize('Banking_System', 'loneranger', 'ashish',{
-  host: 'localhost',
+module.exports = new Sequelize('bjjyso0xmhmdimi90fmy', 'ulfnorb6ywwesy5m', '3qq9mtMD50P6Jmf2em1j',{
+  host: 'bjjyso0xmhmdimi90fmy-mysql.services.clever-cloud.com',
   dialect: 'mysql',
 
   pool:{
